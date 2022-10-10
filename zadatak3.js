4901aba86ee4f782a343f69887aeb7d11dbb41ff
AndyLufic <andreaslucic9@gmail.com>
2022-10-10 13:10:32 +0200

    rjesen zadatak 3

---
+++ zadatak3.js
@@ -0,0 +1,18 @@
+function sati(x){
+    let sat = 0
+    let minuta = 0
+
+    while (! x<60){
+        sat +=1; 
+        x-=60
+        if(x < 60 ){
+            minuta += x; 
+
+        }
+        
+    }
+   
+    console.log("ima ", sat , "sati i ", minuta, "minuta " )
+}
+
+sati(123)
