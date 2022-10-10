70d23c11305ef84bad5d9c0c04f129983abcb8a1
AndyLufic <andreaslucic9@gmail.com>
2022-10-10 12:50:20 +0200

    rjesen zadatak 2

---
+++ zadatak2.js
@@ -0,0 +1,12 @@
+function unutar(broj){
+    if (broj >=100 & broj <=150000){
+        console.log(" broj je u zadanom spektru")
+
+    }
+    else{
+        console.log("broj nije unutar spektra")
+
+    } 
+}
+
+unutar(1546)
