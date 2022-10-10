4c05158e20bd3142866340774ba5af7f7397711f
AndyLufic <andreaslucic9@gmail.com>
2022-10-10 13:40:29 +0200

    rjesen zadatak 7

--- zadatak7.js
+++ zadatak7.js
@@ -1,6 +1,12 @@
-polje[1,2,3,4]
-
+polje= [1,2,3,4]
+polje2 = []
 function obrnut(){
-    
+    for (let i of polje ){
+        polje2.push(i)
+        polje.pop()
+
+    }
 
 }
+obrnut()
+console.log(polje2)
